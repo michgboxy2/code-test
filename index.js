4a8be130d92c436c728679cb83bf0b28d583510b
michgboxy <michgboxy@gmail.com>
2019-12-05 15:58:48 +0100

    instantiate express

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const eventRoutes = require('./routes/events');
+const app = express();
 
 app.use(cors());
 
