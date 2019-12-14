57c6ce28b635ed6c73484c86f77b7e11990a670e
michgboxy <michgboxy@gmail.com>
2019-12-14 15:19:56 +0100

    refactors index file

--- index.js
+++ index.js
@@ -1,24 +1,5 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const eventRoutes = require('./routes/events');
-const app = express();
-
-app.use(cors());
-
-app.use(express.json());
-app.use(
-  express.urlencoded({
-    extended: true,
-  }),
-);
-
-app.use('/', eventRoutes);
-
-
+const app     = require('./server/server');
 
 const PORT = process.env.PORT || 5000;
-
-
 
 app.listen(PORT, () => console.log('Application listening in port ', PORT));
