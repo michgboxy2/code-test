5666b88db8f8b658fb568741726b30bd7ff9b689
michgboxy <michgboxy@gmail.com>
2019-12-14 15:23:15 +0100

    event model to accomodate the user making the event

--- routes/events.js
+++ routes/events.js
@@ -1,9 +1,15 @@
-const router = require('express').Router();
-const {addEvent, getAllEvents} = require('../controllers/EventController');
+const router = require("express").Router();
+const { decodeToken } = require("../auth/Auth");
+const {
+  addEvent,
+  getAllEvents,
+  getSignInUserEvent,
+  findOneUserEvent
+} = require("../controllers/EventController");
 
-router.route('/').post(addEvent);
-router.route('/').get(getAllEvents);
-
+router.route("/").post(decodeToken, addEvent);
+router.route("/").get(getAllEvents);
+router.route('/user').get(decodeToken, getSignInUserEvent);
+router.route('/user/:id').get(findOneUserEvent);
 
 module.exports = router;
-
